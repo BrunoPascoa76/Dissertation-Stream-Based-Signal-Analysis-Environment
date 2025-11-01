93f2b2814f40dc9d463bb9d7b213f90b31f13995
BrunoPascoa76 <brunopascoa03@ua.pt>
2025-11-01 15:36:01 +0000

    it's working

--- consumers/wpm.py
+++ consumers/wpm.py
@@ -18,7 +18,7 @@ logger = logging.getLogger("consumer_wpm")
 
 def connect_db():
     client=InfluxDBClient(
-        url="influxdb:8086",
+        url="http://influxdb:8086",
         token=os.environ["INFLUXDB_ADMIN_TOKEN"],
         org="dissertation"
     )
@@ -30,18 +30,18 @@ def process_event(event,write_api:WriteApi):
     timestamp_ns=event.get("timestamp_ns")
     
     if key_class=="WHITESPACE":
-        point=Point("word").tag("sensor","keyboard").time(timestamp_ns,write_precision="ns") #create a new point
+        point=Point("word").tag("sensor","keyboard").field("value",1).time(timestamp_ns,write_precision="ns") #create a new point (value is just because it's needed in the queries)
         logger.info(f"sent word at {timestamp_ns}")
         write_api.write(bucket=BUCKET,record=point) #record it
     elif key_class=="DELETE":
-        point=Point("delete").tag("sensor","keyboard").time(timestamp_ns,write_precision="ns")
+        point=Point("delete").tag("sensor","keyboard").field("value",1).time(timestamp_ns,write_precision="ns")
         logger.info(f"sent delete at {timestamp_ns}")
         write_api.write(bucket=BUCKET,record=point)
 
 if __name__=="__main__":
     consumer=KafkaConsumer(
         TOPIC,
-        bootstrap_servers=['kafka:9092'],  
+        bootstrap_servers=['kafka:29092'],  
         value_deserializer=lambda v: json.loads(v.decode("utf-8")),
         auto_offset_reset="latest",
         enable_auto_commit=True
