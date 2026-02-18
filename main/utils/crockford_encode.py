127f5483665cd191163b6e4fd82cf55107a7845d
BrunoPascoa76 <brunopascoa03@ua.pt>
2026-02-18 14:39:07 +0000

    removed test print from encode

---
+++ main/utils/crockford_encode.py
@@ -0,0 +1,38 @@
+import socket
+import struct
+from base32_crockford import encode #crockford is a variant of base32 that removes often mistyped letters from the dictionary to avoid typos during manual writing
+
+def get_local_ip()->str:
+    """
+    Returns the (primary) local ipv4 addres
+    
+    :return: the address
+    :rtype: str
+    """
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.connect(("8.8.8.8", 80)) #just to trigger route resolution
+        return s.getsockname()[0]
+
+def address_encode(port:int)->str:
+    """
+    Generates a pairing code using crockford encocode
+    :param port: The port to be accessed
+    :type port: int
+    :return: A pairing code including the address, port, version byte and checksum
+    :rtype: str
+    """
+    ip_str = get_local_ip()
+    ip_bytes = socket.inet_aton(ip_str)
+    port_bytes = struct.pack("!H", port)
+    version_byte = struct.pack("!B", 1)
+
+    data = ip_bytes + port_bytes + version_byte  # 7 bytes
+
+    # Convert bytes to integer
+    value = int.from_bytes(data, byteorder="big")
+
+    # Encode with checksum
+    code = encode(value, checksum=True)
+    
+    return "-".join([code[i:i+4] for i in range(0, len(code), 4)]) #groups separated by "-" to be more human readable
+
