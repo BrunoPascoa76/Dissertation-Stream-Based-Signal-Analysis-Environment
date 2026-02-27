4c9c5af7a26baa11b1d86b35940d82d4237f6354
BrunoPascoa76 <brunopascoa03@ua.pt>
2026-02-27 13:32:36 +0000

    it works now, it sends stuff to kafka

--- server/conf/start-connect.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Install MQTT connector if missing
-if [ ! -d /usr/share/java/kafka-connect-mqtt ]; then
-  confluent-hub install --no-prompt confluentinc/kafka-connect-mqtt:latest
-fi
-
-# Start Kafka Connect
-/etc/confluent/docker/run
