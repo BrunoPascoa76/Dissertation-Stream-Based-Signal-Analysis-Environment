fddc4e26739fd6ca405d242f96e46bbcc843745e
BrunoPascoa76 <brunopascoa03@ua.pt>
2026-03-06 17:28:38 +0000

    pre-testing commit

--- client/main.py
+++ client/main.py
@@ -6,50 +6,21 @@ from pluggy import PluginManager
 from utils.BasePlugin import BasePlugin
 from utils.MQTTHelper import MQTTHelper
 from plugins.core.MosquittoManager import MosquittoManager
+from widgets.SensorSelector import SensorSelector
 
-class SensorsDesktopApp(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-    
-    def initUI(self):
-        # Set window properties
-        self.setWindowTitle('Hello World App')
-        self.setGeometry(100, 100, 300, 200)  # x, y, width, height
-        
-        # Create layout
-        layout = QVBoxLayout()
-        
-        # Create label with hello world text
-        label = QLabel('Hello World!')
-        label.setStyleSheet('font-size: 24px; font-weight: bold;')
-        layout.addWidget(label)
-        
-        # Set the layout on the application's window
-        self.setLayout(layout)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     
-    #register plugin Manager
-    app.pm=PluginManager("sensorsDesktop")
-    app.pm.add_hookspecs(BasePlugin)
-    
+    #add mosquitto docker manager
     app.mosquitto=MosquittoManager()
     app.mosquitto.start()
     
-    #register sensor plugins
-    publisher=MQTTHelper()  
-    sensors = entry_points(group='plugins.sensors')
     
-    for entry_point in sensors:
-        plugin_class = entry_point.load()
-        plugin_instance = plugin_class(publisher)
-        app.pm.register(plugin_instance)
     
     
     # Create and show the application's window
-    ex = SensorsDesktopApp()
+    ex = SensorSelector()
     ex.show()
     
     app.aboutToQuit.connect(app.mosquitto.stop)
---
+++ client/widgets/SensorSelector.py
@@ -0,0 +1,74 @@
+from importlib.metadata import entry_points
+
+from PyQt6.QtWidgets import QApplication, QCheckBox, QVBoxLayout, QWidget
+from PyQt6.QtCore import QSettings
+from PyQt6.QtGui import QFont
+from pluggy import PluginManager
+
+from utils.setupLogger import setup_logger
+from utils.BasePlugin import BasePlugin
+from utils.MQTTHelper import MQTTHelper
+
+class SensorSelector(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.logger=setup_logger("SensorSelector")
+        self.app=QApplication.instance()
+        self.settings = QSettings("Dissertation", "SensorsDesktop")
+        
+        self.disabled_sensors = self.settings.value("disabled_sensors", type=list) or []
+        self._load_available_sensors()
+        
+        self.initUI()
+    
+    def _load_available_sensors(self):
+        """register sensor sensor"""
+        self.app.pm=PluginManager("sensorsDesktop")
+        self.app.pm.add_hookspecs(BasePlugin)
+        
+        publisher=MQTTHelper()  
+        _entry_points = entry_points(group='plugins.sensors')
+        self.available_sensors=dict()
+        self.logger.debug(f"loaded entry_points: {(e.name for e in _entry_points)}")
+    
+        for sensor in _entry_points:
+            sensor_name=sensor.name
+            sensor_class=sensor.load()
+            sensor_instance=sensor_class(publisher)
+            self.available_sensors[sensor_name]=sensor_instance
+            
+            if sensor_name not in self.disabled_sensors:
+                self.app.pm.register(sensor_instance)
+                self.logger.debug(f"registered sensor {sensor_name}")
+            
+            
+    def _toggle_sensor(self, sensor_name):
+        """Toggle a single sensor's state"""
+        try:
+            sensor=self.available_sensors[sensor_name]
+            
+            if sensor_name in self.disabled_sensors:
+                self.disabled_sensors.remove(sensor_name)
+                self.app.pm.register(sensor)
+                self.logger.info(f"enabled sensor: {sensor_name}")
+            else:
+                self.disabled_sensors.append(sensor_name)
+                self.app.pm.unregister(sensor_name)
+                self.logger.info(f"disabled sensor: {sensor_name}")
+        except KeyError:
+            self.logger.error(f"Sensor not found: {sensor_name}")
+            
+        
+        self.settings.setValue("disabled_plugins", self.disabled_sensors)
+        
+    def initUI(self):
+        layout = QVBoxLayout()
+    
+        for sensor_name in self.available_sensors:
+            cb = QCheckBox(sensor_name)
+            cb.setChecked(sensor_name not in self.disabled_sensors)
+            cb.stateChanged.connect(lambda state, name=sensor_name: self._toggle_sensor(name))
+            layout.addWidget(cb)
+
+        self.setLayout(layout)
+    
