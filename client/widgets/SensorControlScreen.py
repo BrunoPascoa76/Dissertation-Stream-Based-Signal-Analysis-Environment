f3a34858712d05e2d199ae099ef0ccefc90b0313
BrunoPascoa76 <brunopascoa03@ua.pt>
2026-05-01 18:22:27 +0100

    changed title

--- client/widgets/SensorControlScreen.py
+++ client/widgets/SensorControlScreen.py
@@ -87,5 +87,10 @@ class SensorControlScreen(QWidget):
             self.button.setText("Start")
             
     def show_visualization(self):
+        self.viz_parent.setWindowTitle("Data visualization")
         self.viz_parent.show()
         self.viz_parent.raise_()
+        
+    def closeEvent(self, event):
+        self.viz_parent.close()
+        event.accept()
